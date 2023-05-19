83087806362ee75b6667898cf8903f14ae74f1f1
KaitouKuga <133513341+KaitouKuga@users.noreply.github.com>
2023-05-19 20:55:50 +0700

    first structure

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function hitungLuas(e) {
+    var sisiLuas = document.getElementById("sisi-luas").value;
+
+    // document.getElementsByClassName("result-luas").
+    document.getElementById("output-luas").innerText = sisiLuas
+    console.log(value)
+}
